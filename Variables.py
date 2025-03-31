05a251909b850bd0a2f9aad3d2e2661b79480f55
martinoum <martinouvillamorales@gmail.com>
2025-03-31 18:09:57 -0300

    variables

---
+++ Variables.py
@@ -0,0 +1,25 @@
+nombre = "Martino"
+edad = 21
+estaturaCm = 170
+
+print("Los datos enviados son: ")
+print("Nombre:",nombre)
+print("Edad:",edad)
+print("Estatura:",estaturaCm,"cm")
+print("---------------------------")
+print("Los tipos de variables de los datos son: ")
+print("De nombre es de tipo:",type(nombre),"De edad es de tipo:",type(edad),"De estatura es de tipo:",type(estaturaCm))
+print("---------------------------")
+
+num1="21"
+num2="21"
+resultado = num1 + num2
+print("El resultado de la suma es:",resultado)
+
+nombre = input("Ingrese su nombre: ")
+print("Hola", nombre)
+
+var1 = input("Enter a number")
+var2 = input("Enter another number")
+resultado = int(var1) + int(var2)
+print("La suma de los dos numeros es:", resultado)
